1fbe917df33f88057d34776ead04f2c7aeef416c
oscar2706 <spat_2706@outlook.com>
2019-04-09 13:55:19 -0500

    Barra de navegación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  images = [1, 2, 3].map(() => `https://picsum.photos/1000/600?random&t=${Math.random()}`);
+}
