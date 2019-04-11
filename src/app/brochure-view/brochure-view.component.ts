c876cf398d2b1eec4974ed2bbb77165efe4a151b
juan_pi_c <juanfloresca16@gmail.com>
2019-04-10 21:47:24 -0500

    vistaConvocatoria

---
+++ src/app/brochure-view/brochure-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brochure-view',
+  templateUrl: './brochure-view.component.html',
+  styleUrls: ['./brochure-view.component.css']
+})
+export class BrochureViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
