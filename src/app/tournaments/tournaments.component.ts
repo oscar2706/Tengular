648d9a1a70aba3675fca2bf7f3e3a3ee2356b1bb
OliverV <oliver10vi@gmail.com>
2019-05-07 00:08:52 -0500

    Paginacion de cards

--- src/app/tournaments/tournaments.component.ts
+++ src/app/tournaments/tournaments.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Tournament } from '../models/tournament.model';
+import { TournamentService } from '../services/tournament.service';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { MockResourceLoader } from '@angular/compiler/testing';
 
 export interface Cat {
   value: string;
@@ -15,12 +19,19 @@ export interface Mod {
   viewValue: string;
 }
 
+export interface More {
+  value:number;
+}
 @Component({
 
   templateUrl: './tournaments.component.html',
   styleUrls: ['./tournaments.component.css']
 })
+
+
 export class TorneosComponent implements OnInit {
+ 
+  tournaments: Tournament[];
   
   cats: Cat[] = [
     {value: 'varonil-0', viewValue: 'Varonil'},
@@ -38,10 +49,24 @@ export class TorneosComponent implements OnInit {
     {value: 'masculino-1', viewValue: 'Masculino'}
   ];
 
-  constructor() { }
+  more:More={
+    value:3
+  }
+
+  constructor(
+    private tournamentService: TournamentService,
+
+    ) { }
+
+  onClick(){
+    this.more.value=this.more.value+3;
+  }
 
   ngOnInit() {
-    
+    this.tournamentService.getTournament().subscribe(tournaments => {
+      this.tournaments = tournaments;
+      console.log(this.tournaments);
+    });
   }
 
 
