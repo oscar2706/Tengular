d081a0f16b14f4f059873887169212599d582d5a
OliverV <oliver10vi@gmail.com>
2019-05-13 13:23:45 -0500

    Informacion completa jugador para internauta

--- src/app/selected-player/selected-player.component.ts
+++ src/app/selected-player/selected-player.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Player } from '../models/player.model';
 import { PlayerService } from '../services/player.service';
+import { MatchService } from "../services/match-single.service";
+import { Match } from '../models/match-single.model';
 
 export interface Match {
   result?: boolean;
@@ -11,19 +13,6 @@ export interface Match {
   matchDate: string;
 }
 
-const NEXT_MATCHES: Match[] = [
-  { opponentName: "Rafael Nadal", round: '1/4 de final', tournament: "Primavera 2019", matchDate: 'Hoy - 16:00' },
-  { opponentName: "Novak Djokovic", round: 'final', tournament: "ATP 100 Miami", matchDate: 'Mañana - 15:00' },
-  { opponentName: "Alexander Zverev", round: '1/8 de final', tournament: "Verano 2019", matchDate: '18/05/2019 - 17:00' },
-];
-
-const MATCHES: Match[] = [
-  { result: true, opponentName: "Rafael Nadal", round: '1/4 de final', tournament: "Primavera 2019", matchDate: 'Ayer - 15:30' },
-  { result: false, opponentName: "Novak Djokovic", round: 'semifinal', tournament: "ATP 1000 Miami", matchDate: '03/03/2019 - 12:30' },
-  { result: true, opponentName: "Alexander Zverev", round: '1/8 de final', tournament: "Verano 2019", matchDate: '03/03/2019 - 13:30' },
-  { result: true, opponentName: "Fabio Fognini", round: '1/4 de final', tournament: "ATP 1000 California", matchDate: '03/03/2019 - 17:30' },
-  { result: false, opponentName: "Milos Raonic", round: 'final', tournament: "Primavera 2019", matchDate: '03/03/2019 - 14:00' },
-];
 @Component({
   selector: 'app-selected-player',
   templateUrl: './selected-player.component.html',
@@ -31,23 +20,35 @@ const MATCHES: Match[] = [
 })
 export class SelectedPlayerComponent implements OnInit {
   players: Player[];
-  displayedColumns: string[] = ['opponentName', 'round', 'tournament', 'matchDate'];
-  sourceNextMatches = NEXT_MATCHES;
-  displayedColumns2: string[] = ['result', 'opponentName', 'round', 'tournament', 'matchDate'];
-  sourceMatches = MATCHES;
+
+  MATCHES: Match[] = [
+
+  ];
+
+  NEXTMATCHES: Match[] = [
+
+  ];
+
+  displayedColumns: string[] = ['player', 'round', 'tournamentName', 'date'];
+  displayedColumns2: string[] = ['player', 'round', 'tournamentName', 'date'];
+  constructor (private route: ActivatedRoute, private playerService: PlayerService, private matchService: MatchService) { }
   
-  constructor(private _route: ActivatedRoute, private playerService: PlayerService) { }
-
-  ngOnInit() {
-
-
-    let nameP = this._route.snapshot.paramMap.get('imageP');
-    this.playerService.getPlayerSelect(nameP).subscribe(players => {
+  ngOnInit () {
+    
+    let nameP = this.route.snapshot.paramMap.get('imageP');
+    this.playerService.getPlayerSelect(nameP).subscribe(players =>{
       this.players = players;
+      
       console.log(this.players);
+      this.matchService.getPlayedMatchesFromPlayer(this.players[0].name).subscribe(matches =>{
+        this.MATCHES=matches;
+        console.log(this.MATCHES) 
+      });
+
+      this.matchService.getNextMatchesFromPlayer(this.players[0].name).subscribe(matches =>{
+        this.NEXTMATCHES=matches;
+      });
     });
 
-    
   }
-
 }
